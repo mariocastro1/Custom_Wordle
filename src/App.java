165cc6146a8489e80b78acb7746c6a829e0a06bb
mariocastro1 <mariocgtablet@gmail.com>
2025-02-14 20:09:26 +0100

    First usable commit

--- src/App.java
+++ src/App.java
@@ -8,11 +8,14 @@ import java.util.Scanner;
 
 
 public class App {
+    
+    private static String words_file = "src/hard_words.txt"; //file with the game words
+    private static int tries = 6;
 
     
-    public static List<String> words_reader() {
+    public static List<String> words_reader() { //reads the words from the file
             try {
-                List<String> ls = Files.readAllLines(Paths.get("src/palabras_iniciales.txt"));
+                List<String> ls = Files.readAllLines(Paths.get(words_file));
                 return ls;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -20,7 +23,53 @@ public class App {
             }
         }
 
+    public static String word_gen() { //generates a randow word
+        List<String> words = words_reader();
+        return words.get((int) (Math.random() * words.size()));
+    }
 
+    public static String ask_word() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose a word: \n");
+        String word = scanner.next();
+        // scanner.close(); 
+        return word;
+    }
+
+
+
+    public static String game() { 
+        String goal = word_gen();
+        String answer ="You lose!";
+        String word = "";
+        String result = "XXXXX";
+        // System.out.println(goal);
+
+        for (int i = 0 ; i <= tries || !answer.equals("You win!") ; i++) {
+            word = ask_word();
+            result = "XXXXX";
+
+            for (int c = 0 ; c < result.length() ; c++) {
+                if (word.substring(c, c+1).equals(goal.substring(c, c+1))) {
+                    result = result.substring(0, c) + "B" + result.substring(c+1);
+                }
+
+                else if (goal.contains(word.substring(c, c+1))){
+                    result = result.substring(0, c) + "O" + result.substring(c+1);
+                }
+            }
+
+            System.out.println(result);
+            if (result.equals("BBBBB")) {
+                answer = "You win!";
+                break;
+            }
+
+        }
+        
+        System.out.println(answer);
+        return null;
+    }
 
 
 
@@ -28,14 +77,6 @@ public class App {
     
     
         public static void main(String[] args) throws Exception {
-            // List<String> word = words_reader();
-            // String chosen_words = word.get((int) (Math.random() * word.size()));
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Introduce un número: ");
-            String intento = scanner.next();
-            System.out.println("palabra introducido: " + intento);
-            scanner.close(); // Cerrar el Scanner
-
+        game();
     }
 }
